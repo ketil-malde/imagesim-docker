b5fa586ddfc2a170c34b51d7607391a8d528e4e8
Ketil Malde <ketil.malde@imr.no>
2020-05-07 12:42:51 +0200

    Iterate over years, gen 5K images as default

--- imagesim.sh
+++ imagesim.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-YEAR=2017
+# number of images to generate from each year
 
-DATA=fishDatasetSimulationAlgo/fish_dataset
-CROPS=${DATA}/${YEAR}/train/crops
-BGS=${DATA}/${YEAR}/train/backgrounds
+N=5000
+for YEAR in 2017 2018; do
 
-mkdir simulated_images
-python3 src/imagesim.py -c ${CROPS} -b ${BGS} -n 10 --masks=True -o simulated_images
+  DATA=fishDatasetSimulationAlgo/fish_dataset
+  CROPS=${DATA}/${YEAR}/train/crops
+  BGS=${DATA}/${YEAR}/train/backgrounds
+  OUT=sim
+  mkdir ${OUT}-${YEAR}
+  python3 src/imagesim.py -c ${CROPS} -b ${BGS} -n ${N} --masks=True -o ${OUT}-${YEAR}
+
+done
